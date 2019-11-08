5ddf1bd81c55cc8ebfb366349b7ff00ee055b284
Aurelian Timu <aurelian.timu@yahoo.com>
2019-11-08 21:11:00 +0200

    Added end_session_endpoint functionality for iOS

---
+++ lib/src/end_session_request.dart
@@ -0,0 +1,33 @@
+import 'service_configuration.dart';
+
+/// The details of an end_session request to initiate a logout
+class EndSessionRequest {
+  EndSessionRequest(this.issuer, this.idTokenHint, this.discoveryUrl,
+      this.redirectUrl, this.serviceConfiguration,
+      {this.additionalParameters});
+
+  String issuer;
+
+  String idTokenHint;
+
+  String discoveryUrl;
+
+  String redirectUrl;
+
+  // The details of the OAuth 2.0 endpoints that can be explicitly when discovery isn't used or not possible
+  ServiceConfiguration serviceConfiguration;
+
+  /// Additional parameters to include in the request
+  Map<String, String> additionalParameters;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'issuer': issuer,
+      'idTokenHint': idTokenHint,
+      'discoveryUrl': discoveryUrl,
+      'redirectUrl': redirectUrl,
+      'serviceConfiguration': serviceConfiguration?.toMap(),
+      'additionalParameters': additionalParameters,
+    };
+  }
+}
