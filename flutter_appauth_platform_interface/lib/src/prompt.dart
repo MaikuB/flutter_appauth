6dd2f6f8f334383ff259587383414d2e31af43aa
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2025-10-26 19:58:51 +1100

    apply requested API doc changes

--- flutter_appauth_platform_interface/lib/src/prompt.dart
+++ flutter_appauth_platform_interface/lib/src/prompt.dart
@@ -1,10 +1,12 @@
 /// All spec-defined values for the OpenID Connect 1.0 `prompt` parameter.
 ///
-/// See OpenID Connect Core 1.0, Section 3.1.2.1 for more infos:
+/// See OpenID Connect Core 1.0, Section 3.1.2.1 for more information:
 /// https://openid.net/specs/openid-connect-core-1_0.html#rfc.section.3.1.2.1
 class Prompt {
   /// The Authorization Server MUST NOT display any authentication or consent
-  /// user interface pages. An error is returned if an End-User is not already
+  /// user interface pages.
+  ///
+  /// An error is returned if an End-User is not already
   /// authenticated or the Client does not have pre-configured consent for the
   /// requested Claims or does not fulfill other conditions for processing the
   /// request. The error code will typically be `login_required`,
@@ -16,17 +18,22 @@ class Prompt {
   static const String none = 'none';
 
   /// The Authorization Server SHOULD prompt the End-User for re-authentication.
+  ///
   /// If it cannot re-authenticate the End-User, it MUST return an error,
   /// typically `login_required`.
   static const String login = 'login';
 
   /// The Authorization Server SHOULD prompt the End-User for consent before
-  /// returning information to the Client. If it cannot obtain consent, it MUST
+  /// returning information to the Client.
+  ///
+  /// If it cannot obtain consent, it MUST
   /// return an error, typically `consent_required`.
   static const String consent = 'consent';
 
   /// The Authorization Server SHOULD prompt the End-User to select a user
-  /// account. This enables an End-User who has multiple accounts at the
+  /// account.
+  ///
+  /// This enables an End-User who has multiple accounts at the
   /// Authorization Server to select amongst the multiple accounts that they
   /// might have current sessions for. If it cannot obtain an account selection
   /// choice made by the End-User, it MUST return an error, typically
